a557831371dc1b94fbcac72b29fc3715d3eda61c
nikolozkariauli <nikolozkariauli20@gmail.com>
2025-07-19 18:21:44 +0400

    first commit

---
+++ day 1/csswork/day 3.py
@@ -0,0 +1,10 @@
+house="roof windov door the won"
+print("house")
+
+dag="book note book pen"
+print("dag")
+
+breakfast="butter bread tea sugar"
+print("breakfast")
+
+
---
+++ day 1/homework/homework.py
@@ -0,0 +1,21 @@
+ronaldo="goal suuuu 939 goal"
+print(ronaldo)
+computer="keydoard screen mouse"
+print(computer)
+a_chair="a tree a nail"
+print(a_chair)
+bed="pillow a tree"
+print(bed)
+age= 10
+print(age)
+#axali davaleba
+mother="mari"
+print(mother)
+brother="bondo"
+print(brother)
+sister="elene lalo nano"
+print(sister)
+grantmother="nino natia"
+print(grantmother)
+grantfather="bidxina dato"
+print(grantfather)
