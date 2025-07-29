308d555fd583ac555f9240d51e8b6256a03aa720
nikolozkariauli <nikolozikariauli20@gmail.com>
2025-07-29 15:49:34 +0400

    “განახლება”

---
+++ day 4/classwork/davaleba.py
@@ -0,0 +1,10 @@
+name = "Alice"
+age = 15
+city = "Paris"
+hobby = "painting"
+
+print("Hello, my name is ""!")
+print("I am ears old.")
+print("I live in city")
+print("My favorite hobby is  hobby")
+print("Nice to meet you!")
