7e9cee4a0ebcf919caa5caff6ac9337593302fe9
nikolozkariauli <nikolozikariauli20@gmail.com>
2025-08-13 17:00:18 +0400

    “განახლება”

---
+++ day 6/cklasswork/davaleba.py
@@ -0,0 +1,8 @@
+num = input("560")
+print(" <class 'str'>", type(num))
+
+num_int = int(num)
+print("<class 'int'>", type(num_int))
+
+num_float = float(num)
+print("<class 'float'>", type(num_float))
