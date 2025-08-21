503a177f1ba7f0b2f30dc2191209fd606b17df5e
nikolozkariauli <nikolozikariauli20@gmail.com>
2025-08-21 22:10:13 +0400

    “განახლება”

---
+++ level 7.5/cklasswork/davaleba 7.py
@@ -0,0 +1,7 @@
+user_input = input("1500")
+
+as_int = int(float(user_input))  
+as_float = float(user_input)
+as_str = str(user_input)
+
+print("as_int")
---
+++ level 7.5/cklasswork/davaleba2.py
@@ -0,0 +1,7 @@
+mun = input("500")
+number_float = float(user_input)
+number_int = int(number_float)
+
+   
+print("500")
+print("500.0")
