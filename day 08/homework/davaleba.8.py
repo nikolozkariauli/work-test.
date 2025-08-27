16aaf95ddaf69618c08cb1fd29e584f82a6af580
nikolozkariauli <nikolozikariauli20@gmail.com>
2025-08-27 10:54:01 +0400

    “განახლება”

---
+++ day 08/homework/davaleba.8.py
@@ -0,0 +1,22 @@
+# Boolean არის მონაცემთა ტიპი, რომელსაც მხოლოდ 2 ორი მდგომარეობა აქვს: True (ჭეშმარიტი) ან False (მცდარი)
+
+# data type მონაცემთა ტიპები არის: str, int, float, bool.
+
+# Binary (ბინარული) არის რიცხვთა სისტემა, რომელიც იყენებს მხოლოდ ორ ციფრს: 0 და 1
+
+# data conversation ფუნქცია არის: str, int, float, bool, list.
+
+is_student = ("false")
+print(is_student == false)
+
+is_super_gela = ("true")
+print(is_super_gela != true)
+
+# print(boolean(0))"es araa swori ai esaa swori"print(bool(0))
+
+# ვეფხვისტყაოსნიდან 1 აბზაცი 10000010 10100000 00100000 10100011 10101110 10100101 00100000
+
+user_input = input("500")
+number = int(user_input)
+number > 18
+print("ჩემი რიცხვი ნაკლებია 500-ზე")
