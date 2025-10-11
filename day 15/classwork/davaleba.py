3d6f535d4417d406b436e6b22dcd7df7e9634b63
nikolozkariauli <nikolozikariauli20@gmail.com>
2025-10-11 22:12:50 +0400

    “განახლება”

---
+++ day 15/classwork/davaleba.py
@@ -0,0 +1,15 @@
+for i in range(1, 101):
+    print(i)
+
+for i in range(50, 101, 2):
+    print(i)
+
+for i in range(1, 51, 5):
+    print(i)
+
+for i in range(4, 9, 2):
+    print(i)
+
+for i in range(10):
+    user_input = input(f"{i+1}.30:")
+    print("30: {user_input}")
