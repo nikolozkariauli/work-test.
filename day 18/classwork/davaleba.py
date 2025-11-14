348c3fb9f663d3fe950859a13957b8baa0efaf30
nikolozkariauli <nikolozikariauli20@gmail.com>
2025-11-14 14:52:28 +0400

    “განახლება”

---
+++ day 18/classwork/davaleba.py
@@ -0,0 +1,19 @@
+
+i = 30
+while i >= 1:
+    print(i)
+    i -= 1
+
+
+
+i = 1
+while i <= 100:
+    print(i)
+    i += 2
+
+
+
+i = 1
+while i <= 50:
+    print(i)
+    i += 3
---
+++ day 18/homework/davaleba.py
@@ -0,0 +1,53 @@
+#1)#i = 1
+#while i <= 10:
+#    print(i)
+#    i += 1
+
+
+#i = 30
+#while i >= 1:
+#    print(i)
+#    i -= i
+
+
+
+#i = 1
+#while i <= 100:
+#    print(i)
+#    i += 2
+
+
+
+#i = 1
+#while i <= 50:
+#    print(i)
+#    i += 3
+
+
+
+
+#2)i = 1
+#while i <= 40:
+#    print(i)
+#    i += 1
+
+#i = 40
+#while i <= 1:
+#    print(i)
+#    i >= 2
+
+
+
+#3)i = 1
+#while i <= 99:
+#    print(i)
+#    i >= 5
+
+
+#4)i = 100
+#while i >= 1:
+#    print(i)
+#    i >= 5
+
+
+
