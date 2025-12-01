5d4cd6b89a077cd9b9cfe6430e52405b62948358
nikolozkariauli <nikolozikariauli20@gmail.com>
2025-12-01 19:51:30 +0400

    “განახლება”

--- day 20/clsswork/davaleba.py
+++ day 20/clsswork/davaleba.py
@@ -1,6 +1,28 @@
-age = int(input("10"))
+#1)name = input("nikolozi")
 
-if age > 18:
-    print("adult")
-else:
-    print("kid")
+#if name == "Aleksandre":
+#    print("Mentor")
+#else:
+#    print("student")
+
+
+
+#2)name = input("nikolozi")
+
+#if name == "ana":
+#    print(3)
+#else:
+#    print("idk")
+
+
+
+#3)first_number = int(input("900"))
+
+#secret_number = 5
+#while True:
+#    guess = int(input("901"))
+#    if guess == secret_number:
+#        print("win")
+#        quit()
+#    else:
+#        print("lose")
---
+++ day 20/homework/davaleba.py
@@ -0,0 +1,48 @@
+#1)age = int(input("10"))
+
+#if age > 18:
+#    print("adult")
+#else:
+#    print("kid")
+
+
+
+#2)name = input("nikolozi")
+
+#if name == "luka":
+#    print("you are luka")
+#else:
+#    print("you are not luka")
+
+
+
+#3)age = int(input("10"))
+#name = input("nikolozi")
+
+#if name == "aleksandre" and age > 18:
+#    print("you are allowed")
+#else:
+#    print("you are not allowed")
+
+
+
+#4)first_number = int(input("500"))
+
+#secret_number = 7
+
+#while True:
+#    guess = int(input("800"))
+#    if guess == secret_number:
+#        print("win")
+#        quit()
+#    else:
+#        print("lose")
+
+
+
+#5)name = input("ნიკოლოზი")
+
+#if name == "Aleksandre":
+#    print("Mentor")
+#else:
+#    print("student")
