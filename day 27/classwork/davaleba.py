7dd8d3eb90316e598e6af2a517349dc69c78e2af
nikolozkariauli <nikolozikariauli20@gmail.com>
2026-01-26 14:35:33 +0400

    “განახლება”

---
+++ day 27/classwork/davaleba.py
@@ -0,0 +1,20 @@
+#1)nums = [1, 2, 3, 4, 5]
+#nums.append(6)
+#nums.append(7)
+#nums.append(8)
+#nums.append(9)
+#nums.append(10)
+#print(nums)
+
+
+
+#2)nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#nums[6]
+#nums.remove(9)
+#print(nums)
+
+
+
+#3)nums = [1, 2, 3, 4, 5, 6, 8, 10]
+#nums.insert(4, "new string") 
+#print(nums)
---
+++ day 27/homework/davaleba.py
@@ -0,0 +1,25 @@
+#1)nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+#print(nums)
+#nums.extend([11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
+#print(nums)
+#for _ in range(5):
+#  nums.pop()
+#print(nums)
+
+
+
+
+#2)nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#print(nums)
+#nums.extend([11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
+#print(nums)
+#nums.extend[6, 7, 8, 9, 10]
+#nums.remove(nums.remove)
+#print(nums)
+
+
+
+
+
+
+
