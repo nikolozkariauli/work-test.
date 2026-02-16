11b33e82c8d6bb2f896f069402841a7caa628dd6
nikolozkariauli <nikolozikariauli20@gmail.com>
2026-02-16 16:47:27 +0400

    “განახლება”

---
+++ day 31/classwork/davaleba.py
@@ -0,0 +1,49 @@
+#1)def print_numbers(): 
+ #for i in range(1, 11):
+#    print(i)
+
+
+
+#2)def print_numbers_while():   
+#    i = 1
+#    while i <= 10:
+#        print(i) 
+#        i += 1
+
+
+
+
+#3)def print_hello_world():  
+#     for i in range(10):
+#          print("hello world")
+
+
+
+
+#4)def print_name():
+#    name = input("nikolozi")
+#    print("nikolozi") 
+#    print()
+
+
+
+#5)def greet(name): 
+#    print("გამარჯობა")
+
+
+
+
+
+#6)def print_name_length(name):
+#    print(name)
+#    print_name_length("ნინო")
+#    print_name_length("გიორგი")
+
+
+
+
+def greet_names(names):
+    for name in names:
+     print("გამარჯობა nikoloz")
+    name_list = ["ნინო", "გიორგი", "მარიამი", "ლევანი"]
+    greet_names(name_list)
---
+++ day 31/homework/davaleba1.py
@@ -0,0 +1,4 @@
+def print_user_name():
+    name = input("nikolozi")
+    print("nikolozi")
+    print_user_name()
