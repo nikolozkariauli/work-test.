60944708450a7f53504788efb6eef05f790d6f82
nikolozkariauli <nikolozikariauli20@gmail.com>
2026-03-10 19:34:22 +0400

    “განახლება”

---
+++ day 33/classwork/davaleba.py
@@ -0,0 +1,97 @@
+#1)cCustom ფუნქციები პროგრამირებაში ძალიან სასარგებლო ინსტრუმენტებია, რადგან ისინი საშუალებას გვაძლევს საკუთარი ლოგიკა და ოპერაციები მოვაქციოთ ერთ reusable ბლოკში.
+
+#2)def greet(name):
+#   return "nika+nika1"
+
+
+
+#print(greet("Nino")) 
+#print(greet("Giorgi"))
+
+
+
+
+#3)def greet(name):
+#    return"nino1+giorgi"
+
+
+
+#print(greet("Nino"))
+#print(greet("Giorgi"))
+
+
+
+#Custom ფუნქციები პროგრამირებაში ერთ-ერთი ყველაზე მნიშვნელოვანი ინსტრუმენტია, რადგან ისინი საშუალებას გვაძლევს კოდი გავხადოთ უფრო ორგანიზებული, მარტივად გასაგები და მრავალჯერადი გამოყენებისთვის მზად.
+
+
+
+#
+# return პროგრამირების ენებში ძალიან მნიშვნელოვანი საკვანძო სიტყვაა, რომელიც გამოიყენება ფუნქციიდან შედეგის უკან დასაბრუნებლად
+
+
+
+#def greet(name):
+#    return  "nino+ giorgi"
+
+
+
+#print(greet("Nino"))
+#print(greet("Giorgi"))
+
+
+
+#def sum_number(num1, num2):
+#    return num1 + num2
+
+
+
+#print(sum_number(5, 7))
+#print(sum_number(10, 20))
+
+
+
+
+
+#def multiply(num1, num2):
+#    return num1 * num2
+
+
+
+#print(multiply(5, 7))
+#print(multiply(10, 20))
+
+
+
+
+
+#def square(num):
+#    return num * num
+
+
+
+#print(square(4))
+#print(square(10))
+
+
+
+
+
+#def odd_or_even(num):
+#   if num % 2 == 0:
+#        return
+#    else:
+#        return
+
+
+
+#print(odd_or_even(4))
+#print(odd_or_even(7))
+
+
+
+
+
+
+
+
+
---
+++ day 34/homework/davaleba.py
@@ -0,0 +1,24 @@
+#def greet(name):
+#    print("გამარჯობა " + name)
+
+#greet("ნინო")
+#greet("გიორგი")
+
+
+
+
+#def multiply_by_two(number):
+#    return number * 2
+
+#print(multiply_by_two(5))
+#print(multiply_by_two(12))
+
+
+
+
+def print_last_char(text):
+    print(text-1)
+
+
+print_last_char("გამარჯობა")
+print_last_char("კომპიუტერი")
